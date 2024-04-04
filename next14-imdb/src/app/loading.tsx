7735b1770c1c128421aa39ad95c223326ab882b6
hslee <vawav11@gmail.com>
2024-04-04 22:35:15 +0900

    로딩 page, svg 적용

---
+++ next14-imdb/src/app/loading.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export default function loading() {
+  return (
+    <div className='flex justify-center mt-16'>
+      <img className="h-52" src='/load.svg' alt='loading...' />
+    </div>
+  );
+}
--- next14-imdb/src/app/page.tsx
+++ next14-imdb/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Results from '@/components/Results';
+import { resolve } from 'path';
 
 const API_KEY = process.env.API_KEY;
 
@@ -12,12 +13,14 @@ interface SearchParams {
 export default async function Home(props: SearchParams) {
   const { searchParams } = props
   const genre = searchParams.genre || 'fetchTrending';
+  
   const res = await fetch(
     `https://api.themoviedb.org/3${
       genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
     }?api_key=${API_KEY}&language=ko-KR&page=1`,
     { next: { revalidate: 10000 } }
   );
+
   const data = await res.json();
   if (!res.ok) {
     throw new Error('Failed to fetch data');
