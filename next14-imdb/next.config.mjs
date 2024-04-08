2713f97f5d41c66f575ee2ce87df053fe54a2e65
hslee <vawav11@gmail.com>
2024-04-08 22:36:56 +0900

    수정

--- next14-imdb/next.config.mjs
+++ next14-imdb/next.config.mjs
@@ -1,22 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'https',
-  //       hostname: 'image.tmdb.org',
-  //       port: '',
-  //       pathname: '**',
-  //     },
-  //   ],
-  // },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'image.tmdb.org',
         port: '', // Leave the port empty unless your image URLs explicitly use a port
-        pathname: '/t/p/original/**', // Adjust the pathname to match the structure of the URLs you are using
+        // pathname: '/t/p/original/**', // Adjust the pathname to match the structure of the URLs you are using
+        pathname: '**', // Adjust the pathname to match the structure of the URLs you are using
       },
     ],
   },
